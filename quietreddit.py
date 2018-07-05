62b14741ae87c8b530a7511dd238d2e892870443
olive247 <clive.unger@gmail.com>
2018-07-04 23:13:22 -0500

    first commit

---
+++ quietreddit.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import praw, config
+
+app = Flask(__name__)
+
+reddit = praw.Reddit(client_id = config.client_id,
+                client_secret = config.client_secret,
+                user_agent = config.user_agent)
+
+@app.route("/")
+
+def index():
+    posts_list  = []
+    for submission in reddit.front.hot(limit = 20):
+        posts_list.append(submission.title)
+    return render_template("layout.html", posts=posts_list)
+
+
+if __name__ == "__main__":
+    app.run()
