da50389a52e3611c9054bd5446d57f034dd271cc
CliveUnger <clive.unger@gmail.com>
2018-07-07 16:27:44 -0500

    display with working link and subreddit reference

--- quietreddit.py
+++ quietreddit.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import praw, config
+import praw, config, pprint
 
 app = Flask(__name__)
 
@@ -7,11 +7,14 @@ reddit = praw.Reddit(client_id = config.client_id,
                 client_secret = config.client_secret,
                 user_agent = config.user_agent)
 
+subreddit_list = 'news+worldnews+business+economics+cryptocurrency'
+multi = reddit.subreddit(subreddit_list)
+
 @app.route("/")
 
 def index():
     posts_list  = []
-    for submission in reddit.front.hot(limit = 30):
+    for submission in multi.hot(limit = 30):
         posts_list.append(submission)
     return render_template("layout.html", posts=posts_list)
 
