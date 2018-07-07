3c32beb0bb8a462acbda4fb099328e8565aa586c
CliveUnger <clive.unger@gmail.com>
2018-07-07 17:30:41 -0500

    add subreddits

--- quietreddit.py
+++ quietreddit.py
@@ -7,7 +7,7 @@ reddit = praw.Reddit(client_id = config.client_id,
                 client_secret = config.client_secret,
                 user_agent = config.user_agent)
 
-subreddit_list = 'news+worldnews+business+economics+cryptocurrency'
+subreddit_list = 'news+worldnews+business+economics+cryptocurrency+science+sports'
 multi = reddit.subreddit(subreddit_list)
 
 @app.route("/")
