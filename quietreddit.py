44fc94b36668d14dd616614d726b9a1a10f90317
CliveUnger <clive.unger@gmail.com>
2018-07-21 00:10:22 -0500

    authentication

--- quietreddit.py
+++ quietreddit.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import praw, config, pprint
 
 app = Flask(__name__)
 
 reddit = praw.Reddit(client_id = config.client_id,
                 client_secret = config.client_secret,
+                redirect_uri=config.redirect_uri+'/authorize',
                 user_agent = config.user_agent)
 
-subreddit_list = 'news+worldnews+business+economics+cryptocurrency+science+sports'
-multi = reddit.subreddit(subreddit_list)
+auth_link = reddit.auth.url(['identity','read'],'...','permanent')
 
 @app.route("/")
-
 def index():
-    posts_list  = []
-    for submission in multi.hot(limit = 30):
-        posts_list.append(submission)
-    return render_template("layout.html", posts=posts_list)
+    posts = []
+    for submission in reddit.front.hot(limit = 30):
+        posts.append(submission)
+    return render_template("layout.html", link=auth_link, posts=posts)
 
+@app.route("/authorize")
+def authorize():
+    state = request.args.get('state', '')
+    code = request.args.get('code', '')
+    print(state)
+    print(code)
+    print(reddit.auth.authorize(code))
+    print(reddit.user.me())
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
